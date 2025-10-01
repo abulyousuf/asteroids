67748c16ae5546bc4e55b44a4dd729cf1e326382
abulyousuf <abulyousuf@outlook.com>
2025-10-01 23:01:35 +0100

    Add shoot cooldown

--- player.py
+++ player.py
@@ -2,11 +2,12 @@ import pygame
 
 from circleshape import CircleShape
 from shot import Shot
-from constants import (PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED)
+from constants import (PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN)
 
 class Player(CircleShape):
   def __init__(self, x, y):
     super().__init__(x, y, PLAYER_RADIUS)
+    self.heat = 0
     self.rotation = 0
 
   # in the player class
@@ -29,6 +30,9 @@ class Player(CircleShape):
     self.position += forward * PLAYER_SPEED * dt
     
   def update(self, dt):
+    if self.heat > 0:
+      self.heat -= dt
+
     keys = pygame.key.get_pressed()
     
     if keys[pygame.K_a]:
@@ -47,5 +51,9 @@ class Player(CircleShape):
       self.shoot()
       
   def shoot(self):
+    if self.heat > 0:
+      return
+    
     shot = Shot(self.position.x, self.position.y)
     shot.velocity = pygame.math.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+    self.heat += PLAYER_SHOOT_COOLDOWN
